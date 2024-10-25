a3e85a7e271e94ed20dfa932f19e89898d481978
Nikhil1qwer <chintufru143@gmail.com>
2024-10-25 07:26:09 +0530

    hii

--- MoviesRec/script.js
+++ MoviesRec/script.js
@@ -1,19 +1,48 @@
-document.getElementById('generateBtn').addEventListener('click', function() {
+const API_KEY = '';
+const BASE_URL = '';
+const POSTER_BASE_URL = ''; // is api provides poster_path
+
+const getMovies = async (genre) => {
+    try {
+        const url = `${BASE_URL}?api_key=${API_KEY}&with_genres=${genre}`;
+        const respone = await fetch(url);
+        // check if promise is brken
+        const data = await respone.json();
+        return data.results; // returns results
+    }catch(e) {
+        console.error('Error during api call: ', e);
+        return [];
+    }
+}
+
+
+document.getElementById('generateBtn').addEventListener('click', function() { // if got data make fn async don't forget
     const searchInput = document.getElementById('searchInput').value;
     const recommendationsContainer = document.getElementById('recommendations');
 
     // Clearing previous results
     recommendationsContainer.innerHTML = '';
 
-    // Example movies data (you can replace this with real data fetching logic)
+    // for testing used
     const movies = [
         { title: 'The Shawshank Redemption', poster: 'https://via.placeholder.com/150', year: 1994 },
         { title: 'The Godfather', poster: 'https://via.placeholder.com/150', year: 1972 },
         { title: 'Inception', poster: 'https://via.placeholder.com/150', year: 2010 }
     ];
 
+    // next step to call the api that returns the data
+    // const movies = await getMovies(searchInput);
+
+    
     // Filtering movies
     const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchInput.toLowerCase()));
+    
+    /** (checking data received)
+     * if(movies.size() == 0) {
+     *      recommendationsContainer.innerHTML = '<p>No movies found for the selected genre.</p>';
+     *      return;   
+     * }
+     */
 
     // Displaying movie cards
     filteredMovies.forEach(movie => {
