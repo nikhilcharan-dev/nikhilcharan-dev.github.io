e6ec28855f9683d0688cc63765397d106c9e79fb
Nikhil Charan <145425611+nikhilcharan-dev@users.noreply.github.com>
2025-05-04 23:39:45 +0530

    Delete VitalDerm_Clinic directory

--- VitalDerm_Clinic/script.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('scroll', () => {
-    let footer = document.querySelector('footer');
-    if(window.scrollY === window.innerHeight) {
-        footer.style.display = 'none';
-    } else {
-        footer.style.display = 'block';
-    }
-});
